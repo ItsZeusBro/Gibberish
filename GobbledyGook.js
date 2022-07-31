369542bebda9049aba2b5318bf57ef991cff0999
Zeus <zeusofolympus1000@gmail.com>
2022-07-31 15:08:08 -0400

    high level view

--- GobbledyGook.js
+++ GobbledyGook.js
@@ -1,25 +1,36 @@
 class RefStack{
   constructor(){
+    //in javascript both objects and arrays are held by reference
     this.refStack=[]
   }
   
-  push(){
-  
+  push(val){
+    this.refStack.push(val)
   }
 
   pop(){
-  
+    return this.refStack.pop()
   }
 }
 
 class Sentinel{
   
   constructor(gook){
+    this.refS=new RefStack()
     this.gook=gook
   }
 
   drill(){
-    //operates like a next() function in that it just gives you the next value in the schema using itercursion
-    //the refStack is used for keeping the context for the sentinel
+    //operates like a next() function on this.gook
+    //when it comes upon anything, it pushes and unbinds
+    //the value from the underlying structure, then returns it
+    if(val !== null){
+      //its more efficient to null check before type checking
+      if(Array.isArray(val)|| typeof val === 'object'){
+        this.refS.push(val)
+        this.unbind(context, val)
+        return val
+      }
+    }
   }
 }
