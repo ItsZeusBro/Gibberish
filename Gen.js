541fd58304dd265863e2d0cba88be41275824fac
Zeus <zeusofolympus1000@gmail.com>
2022-08-01 15:50:44 -0400

    i think thats all we need

--- Gen.js
+++ Gen.js
@@ -96,16 +96,43 @@ class GuardGen{
     }
 }
 
-var gen = new GuardGen(5, 5)
-// gen.log(gen.g)
+// var gen = new GuardGen(5, 5)
+// // gen.log(gen.g)
 
-var gg = new Gobbledy(gen.g)
 
-while(true){
-  var next = gg.next()
-  if(next){
-    console.log(next,'\n\n\n\n')
-  }else{
-    break
+
+var gook = [
+  {
+    'a':[
+      {
+        'b':[1, {}]
+      },
+      {
+        'c':[{}],
+        'd':[{}]
+      }
+    ]
+  },
+  {
+    'e':[
+      {
+        'f':[1, {}]
+      },
+      {
+        'g':[{}],
+        'h':[{}]
+      }
+    ]
   }
-}
+]
+
+var gg = new Gobbledy(gook)
+gg.log(gg.queue)
+// while(true){
+//   var next = gg.next(3)
+//   if(next[0]){
+//     console.log(next,'\n\n\n\n')
+//   }else{
+//     break
+//   }
+// }
--- GobbledyGook.js
+++ GobbledyGook.js
@@ -1,3 +1,5 @@
+import * as util from "node:util"
+
 export class Gobbledy{
   
   constructor(gook){
@@ -5,8 +7,25 @@ export class Gobbledy{
     this.gobbledy(gook)
   } 
 
-  gobbledy(gook){
-    if(gook){
+  gobbledy(gook, n){
+    if(gook && n){
+      if(Array.isArray(gook)){
+        this.queue.push(gook)
+        for(var i = 0; i<gook.length; i++){
+          this.gobbledy(gook[i], n-1)
+        }
+      }else if(typeof gook === 'object'){
+        this.queue.push(gook)
+        var keys = Object.keys(gook)
+        if(keys.length){
+          for(var i = 0; i<keys.length; i++){
+            this.gobbledy(gook[keys[i]], n-1)
+          }
+        }
+      }else{
+        this.queue.push(gook)
+      }
+    }else{
       if(Array.isArray(gook)){
         this.queue.push(gook)
         for(var i = 0; i<gook.length; i++){
@@ -27,37 +46,28 @@ export class Gobbledy{
     return
   }
 
-  next(){
-    return this.queue.shift()
+  next(n){
+    var q = []
+    for(var i=0; i<n; i++){
+      q.push(this.queue.shift())
+    }
+    return q
+  }
+  log(obj){
+    if(obj){
+        console.log(util.inspect(obj, false, null, true))
+    }
+}
+
+  validate(){
+    //we want to validate schema vertically using recursion and some schema pattern matching
+
   }
 }
 
 
 
-// var gook = [
-//   {
-//     'a':[
-//       {
-//         'b':[1, {}]
-//       },
-//       {
-//         'c':[{}],
-//         'd':[{}]
-//       }
-//     ]
-//   },
-//   {
-//     'e':[
-//       {
-//         'f':[1, {}]
-//       },
-//       {
-//         'g':[{}],
-//         'h':[{}]
-//       }
-//     ]
-//   }
-// ]
+
 
 // var gg = new Gobbledy(gook)
 // console.log(gg.queue)
