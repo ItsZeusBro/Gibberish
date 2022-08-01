789ac5455e3a2594e3930833897a2c6c9f1beeda
Zeus <zeusofolympus1000@gmail.com>
2022-07-31 23:39:27 -0400

    thats not how it was logging

--- GobbledyGook.js
+++ GobbledyGook.js
@@ -1,59 +1,59 @@
-class RefStack{
-  constructor(){
-    //in javascript both objects and arrays are held by reference
-    this.refStack=[]
-  }
-  
-  push(val){
-    this.refStack.push(val)
-  }
-
-  pop(){
-    return this.refStack.pop()
-  }
-}
-
-class Sentinel{
+class Gobbledy{
   
   constructor(gook){
-    this.refS=new RefStack()
-    this.gook=gook
-    this.val = gook
-  }
+    this.queue=[]
+    this.gobbledy(gook)
+  } 
 
-  drill(){
-    //sentinel should always keep the construct that it returns the
-    //value from
-    //so when its not an array or obj, it needs to end up
-    //being the lowest level array or obj it obtained the value from
-    
-  }
-  _drill(val){
-    //this drills until it finds 
-    if(val !== null){
-      //its more efficient to null check before type checking
-      if(Array.isArray(val)){
-        this.refS.push(val)
-        val = this.updateFromArr(val)
-        return val
-      }
-      if(typeof val === 'object'){
-        this.refS.push(val)
-        val = this.updateFromObj(val)
-        return val
+  gobbledy(gook){
+
+    if(gook){
+      if(Array.isArray(gook)){
+        for(var i = 0; i<gook.length; i++){
+          this.queue.push(gook)
+          this.gobbledy(gook[i])
+        }
+      }else if(typeof gook === 'object'){
+        var keys = Object.keys(gook)
+        for(var i = 0; i<keys.length; i++){
+          this.queue.push(gook)
+          this.gobbledy(gook[keys[i]])
+        }
+      }else{
+        //raw type
+        this.queue.push(gook)
       }
     }
+    return
   }
 
-  unbindArr(arr){
-    //unbinds the first val in the array
-    //returns the val
-    return val
-  }
-
-  unbindObj(obj){
-    //unbinds the first associated val from Object.keys(obj)
-    //returns that value that was unbound
-    return val
-  }
 }
+
+
+var gook = [
+  {
+    'a':[
+      {
+        'a':[1, {}]
+      },
+      {
+        'b':[{}],
+        'a':[{}]
+      }
+    ]
+  },
+  {
+    'a':[
+      {
+        'a':[1, {}]
+      },
+      {
+        'b':[{}],
+        'a':[{}]
+      }
+    ]
+  }
+]
+
+var gg = new Gobbledy(gook)
+console.log(gg.queue)
