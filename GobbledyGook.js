a5be89337b9da388cc973411f25144d4cff6266a
Zeus <zeusofolympus1000@gmail.com>
2022-07-31 23:47:14 -0400

    that seems to work fine now

--- GobbledyGook.js
+++ GobbledyGook.js
@@ -6,19 +6,22 @@ class Gobbledy{
   } 
 
   gobbledy(gook){
-
     if(gook){
+      console.log(gook)
       if(Array.isArray(gook)){
+        this.queue.push(gook)
         for(var i = 0; i<gook.length; i++){
-          this.queue.push(gook)
           this.gobbledy(gook[i])
         }
       }else if(typeof gook === 'object'){
+        this.queue.push(gook)
         var keys = Object.keys(gook)
-        for(var i = 0; i<keys.length; i++){
-          this.queue.push(gook)
-          this.gobbledy(gook[keys[i]])
+        if(keys.length){
+          for(var i = 0; i<keys.length; i++){
+            this.gobbledy(gook[keys[i]])
+          }
         }
+
       }else{
         //raw type
         this.queue.push(gook)
