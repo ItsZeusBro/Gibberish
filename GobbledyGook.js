6422ce37c51454dd78b35651b7effc0622d297f1
Zeus <zeusofolympus1000@gmail.com>
2022-08-01 15:58:43 -0400

    wow!

--- GobbledyGook.js
+++ GobbledyGook.js
@@ -2,54 +2,38 @@ import * as util from "node:util"
 
 export class Gobbledy{
   
-  constructor(gook){
+  constructor(gook, n){
     this.queue=[]
-    this.gobbledy(gook)
+    this.gobbledy(gook, n)
   } 
 
-  gobbledy(gook, n){
-    if(gook && n){
-      if(Array.isArray(gook)){
-        this.queue.push(gook)
-        for(var i = 0; i<gook.length; i++){
-          this.gobbledy(gook[i], n-1)
-        }
-      }else if(typeof gook === 'object'){
-        this.queue.push(gook)
-        var keys = Object.keys(gook)
-        if(keys.length){
-          for(var i = 0; i<keys.length; i++){
-            this.gobbledy(gook[keys[i]], n-1)
+  gobbledy(gook, n=Infinity){
+      if(gook && n){
+          if(Array.isArray(gook)){
+              this.queue.push(gook)
+              for(var i = 0; i<gook.length; i++){
+                  this.gobbledy(gook[i], n-1)
+              }
+          }else if(typeof gook === 'object'){
+              this.queue.push(gook)
+              var keys = Object.keys(gook)
+
+              if(keys.length){
+                for(var i = 0; i<keys.length; i++){
+                    this.gobbledy(gook[keys[i]], n-1)
+                }
+              }
+          }else{
+              this.queue.push(gook)
           }
-        }
-      }else{
-        this.queue.push(gook)
       }
-    }else{
-      if(Array.isArray(gook)){
-        this.queue.push(gook)
-        for(var i = 0; i<gook.length; i++){
-          this.gobbledy(gook[i])
-        }
-      }else if(typeof gook === 'object'){
-        this.queue.push(gook)
-        var keys = Object.keys(gook)
-        if(keys.length){
-          for(var i = 0; i<keys.length; i++){
-            this.gobbledy(gook[keys[i]])
-          }
-        }
-      }else{
-        this.queue.push(gook)
-      }
-    }
     return
   }
 
   next(n){
     var q = []
     for(var i=0; i<n; i++){
-      q.push(this.queue.shift())
+        q.push(this.queue.shift())
     }
     return q
   }
@@ -65,18 +49,29 @@ export class Gobbledy{
   }
 }
 
-
-
-
-
-// var gg = new Gobbledy(gook)
-// console.log(gg.queue)
-
-// while(true){
-//   var next = gg.next()
-//   if(next){
-//     console.log(next)
-//   }else{
-//     break
-//   }
-// }
+var gook = [
+  {
+    'a':[
+      {
+        'b':[1, {}]
+      },
+      {
+        'c':[{}],
+        'd':[{}]
+      }
+    ]
+  },
+  {
+    'e':[
+      {
+        'f':[1, {}]
+      },
+      {
+        'g':[{}],
+        'h':[{}]
+      }
+    ]
+  }
+]
+var g = new Gobbledy(gook, 1)
+g.log(g.queue)
