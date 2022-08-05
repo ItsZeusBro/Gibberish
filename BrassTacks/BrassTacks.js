8547f3c36c2b613f5432e567d4ef6b6b5d4d5332
Zeus <zeusofolympus1000@gmail.com>
2022-08-04 23:24:31 -0400

    that works

--- BrassTacks/BrassTacks.js
+++ BrassTacks/BrassTacks.js
@@ -1,14 +1,14 @@
 export class BrassTacks{
     constructor(levels, limit){
-        this.tree={}
-        this.limit=limit
-        this.bStrings=[]
-        this.dStrings=[]
-        this.BStrings('1', levels)
-        this.BTT(this.tree)
-        this.dStrings = this.dStrings.sort(function(a, b) {
-            return a - b;
-        })
+        // this.tree={}
+        // this.limit=limit
+        // this.bStrings=[]
+        // this.dStrings=[]
+        // this.BStrings('1', levels)
+        // this.BTT(this.tree)
+        // this.dStrings = this.dStrings.sort(function(a, b) {
+        //     return a - b;
+        // })
     }
     BTT(tree){
         for(var i = 0; i<this.bStrings.length; i++){
@@ -73,25 +73,35 @@ export class BrassTacks{
     }
 
     isRecursive(string){
-        var subStr1=""
+        var subStr1=string[0]
         var subStr2=""
-        for(var i = 0; i<string.length; i++){
+        if(string[0]==string[1]){
+            return true
+        }
+        for(var i = 1; i<string.length; i++){
             //we want the first token which is defined as when the first char
             //no longer equals the iterative char, and terminate when the iterative 
             //char again equals the first char
             subStr1+=string[i]
-            if(subStr1[i]==subStr1[i-1]){
-                for(var j=i+1; j<string.length; j++){
+            if(subStr1[i]!=subStr1[i-1]){
+                //this means the context changed, but subStr1 is not done
+                for(var j = i+1; j<string.length; j++){
                     subStr1+=string[j]
                     if(subStr1[j]!=subStr1[j-1]){
-                        for(var l=j+1; l<string.length; l++){
-                            subStr2+=string[l]
-                            if(subStr1==subStr2){
+                        subStr2+=subStr1[subStr1.length-1]
+                        subStr1 = subStr1.slice(0, -1)
+                        //now we fill substr2
+                        for(var k=j+1; k<string.length; k++){
+                            subStr2+=string[k]
+                            if(subStr1===subStr2){
+
                                 return true
                             }
                         }
+                        return false
                     }
                 }
+                return false
             }
         }
         return false
@@ -118,4 +128,4 @@ export class BrassTacks{
 
 
 var bt = new BrassTacks(5, 10)
-console.log(bt.isRecursive('110110110'))
+console.log(bt.isRecursive('1000010000'))
