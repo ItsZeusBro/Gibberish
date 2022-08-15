8fa91f8b55deaf67a0c031c0cfc60179f80801b8
Zeus <107733608+ItsZeusBro@users.noreply.github.com>
2022-08-15 04:06:38 -0400

    Update and rename GobbledyGen.js to GibberishGen.js

--- GibberishGen.js
+++ GibberishGen.js
@@ -2,7 +2,7 @@ import * as util from "node:util"
 import * as assert from "node:assert"
 import {Test} from "test/Test.js"
 
-class GobbledyGen{
+class GibberGen{
 	constructor(invalid=false, max_depth=10, max_width=10, dist=0.5, pk=['payload']){
 		//dist is the array to object distribution
 		this.invalid=invalid;
@@ -10,10 +10,10 @@ class GobbledyGen{
 		this.max_width=max_width;
 		this.dist=dist;
 		this.pk=pk;
-		this.gg=this.gobbledyGen(max_depth, max_width);
+		this.gg=this.gibberGen(max_depth, max_width);
 	}
 
-	gobbledyGen(max_depth, max_width){
+	gibberGen(max_depth, max_width){
 
 	}
 }
