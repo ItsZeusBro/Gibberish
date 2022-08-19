e1ef72fd3c395367bd497e27a8cb6e35f1f89ef8
Zeus <107733608+ItsZeusBro@users.noreply.github.com>
2022-08-19 12:29:32 -0400

    Update GibberGen.js

--- GibberGen.js
+++ GibberGen.js
@@ -2,17 +2,8 @@ import * as util from "node:util"
 import * as assert from "node:assert"
 import {Test} from "test/Test.js"
 
-class GibberGen{
-	strata(){
-		//can start with an array or an object (because its recursive)
-		//this can be used for matricies if there are no payload keys
-	}
-
-	nested(){
-		//nested objects (we did this in Test library)
-	}
+class StrataGen{
 
 }
 
-console.log(new Test().randInt(5))
 
